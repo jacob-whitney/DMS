8c1948238285ab4d549df131ba5e918bc6d96822
Jacob Whitney <jacob@jacobdellprec.>
2025-02-23 12:21:30 -0500

    Add importing methods

--- src/Main.java
+++ src/Main.java
@@ -29,48 +29,60 @@ public class Main {
             switch (input) {
                 case "i":
                     // Import characters
-                    ArrayList<String> importList = Menu.readFile("resources/char-3.txt");
+                    ArrayList<String> importChars = Menu.readFile(Menu.inputFilePath());
+                    String charLine = "";
+
+                    for (int i = 0; i < importChars.size(); i++) {
+                        charLine = importChars.get(i);
+                        if (Menu.validateImportedString(charLine)) {
+                            list.addCharacter(Menu.parseAttributesFromString(charLine, list));
+                        }
+                    }
+
+                    // Read the file
+                    // Store the data
+                    // Per line
+                    // - Parse data into attributes
+                    // - Validate each attribute
+                    // - Create character
+
+                    /*ArrayList<String> importList = Menu.readFile("resources/char-3.txt");
                     for (int i = 0; i < importList.size(); i++) {
                         System.out.println(importList.get(i));
-                    }
+                    }*/
                     break;
                 case "c":
                     // Create character
+                    int id = Menu.getValidId(Menu.inputAttribute("id"), list);
+                    String name = Menu.getValidName(Menu.inputAttribute("name"), list);
+                    String classification = Menu.getValidClassification(Menu.inputAttribute("classification"));
+                    String race = Menu.getValidRace(Menu.inputAttribute("race"));
+                    int str = Menu.getValidAbilityScore("str", Menu.inputAttribute("str"));
+                    int dex = Menu.getValidAbilityScore("dex", Menu.inputAttribute("dex"));
+                    int con = Menu.getValidAbilityScore("con", Menu.inputAttribute("con"));
 
-                    // User inputs id
-                    // Validate id to meet constraints
-                    // Check id for duplicates
-                    // int id gets valid ID
-
-
-
-                    int id = Menu.getValidId(Menu.inputAttribute("id", sc), list, sc);
-                    String name = Menu.getValidName(sc);
-                    String classification = Menu.getValidClassification(sc);
-                    String race = Menu.getValidRace(sc);
-                    int str = Menu.getValidAbilityScore("str", sc);
-                    int dex = Menu.getValidAbilityScore("dex", sc);
-                    int con = Menu.getValidAbilityScore("con", sc);
-
-
-                    System.out.println("Creating character:"); // --will need to be print headers and print values below on one line
-                    System.out.println("- ID: " + id);
-                    System.out.println("- Name: " + name);
-                    System.out.println("- Class: " + classification);
-                    System.out.println("- Race: " + race);
-                    System.out.println("- Strength: " + str);
-                    System.out.println("- Dexterity: " + dex);
-                    System.out.println("- Constitution: " + con);
+                    System.out.println();
+                    System.out.println("Creating character..."); // --will need to be print headers and print values below on one line
+                    System.out.println("  - ID: " + id);
+                    System.out.println("  - Name: " + name);
+                    System.out.println("  - Class: " + classification);
+                    System.out.println("  - Race: " + race);
+                    System.out.println("  - Strength: " + str);
+                    System.out.println("  - Dexterity: " + dex);
+                    System.out.println("  - Constitution: " + con);
 
                     list.addCharacter(new Character(id, name, classification, race, str, dex, con));
                     break;
+
                 case "p":
                     System.out.println(list.getCharacterList());
                     break;
+
                 case "q":
                     System.out.println("Quitting program...");
                     loop = false;
                     break;
+
                 default:
                     System.out.println("Invalid input, please try again");
             }
--- src/Menu.java
+++ src/Menu.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Menu {
+    private static final Scanner sc = new Scanner(System.in);
+
     // User Input Methods
     /**
      * --method:
@@ -20,20 +22,10 @@ public class Menu {
      * --return:
      * --purpose:
      */
-    public static ArrayList<String> readFile(String filepath) {
-        ArrayList<String> data = new ArrayList<String>();
-        try {
-            Scanner sc = new Scanner(new File(filepath));
-
-            while (sc.hasNextLine()) {
-                data.add(sc.nextLine());
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
-        }
-        return data;
+    public static String inputFilePath() {
+        System.out.println("Enter the file path (ex. resources/chars.txt) you want to import");
+        System.out.println("Filepath: ");
+        return sc.nextLine();
     }
 
     /**
@@ -42,7 +34,7 @@ public class Menu {
      * --return:
      * --purpose:
      */
-    public static String inputAttribute(String attribute, Scanner scanner) {
+    public static String inputAttribute(String attribute) {
         boolean loop = true;
         while (loop) {
             switch (attribute) {
@@ -55,10 +47,29 @@ public class Menu {
                     loop = false;
                     break;
                 case "classification":
+                    System.out.println();
+                    System.out.println("Choose from one of the following classes.");
+                    System.out.println("  1. Barbarian");
+                    System.out.println("  2. Fighter");
+                    System.out.println("  3. Ranger");
+                    System.out.println("  4. Rogue");
+                    System.out.println("  5. Sorcerer");
+                    System.out.println("  6. Warlock");
+                    System.out.println("  7. Wizard");
+                    System.out.println();
                     System.out.print("Class: ");
                     loop = false;
                     break;
                 case "race":
+                    System.out.println();
+                    System.out.println("Choose from one of the following races.");
+                    System.out.println("  1. Dwarf");
+                    System.out.println("  2. Dragonborn");
+                    System.out.println("  3. Elf");
+                    System.out.println("  4. Gnome");
+                    System.out.println("  5. Halfling");
+                    System.out.println("  6. Human");
+                    System.out.println();
                     System.out.print("Race: ");
                     loop = false;
                     break;
@@ -78,9 +89,149 @@ public class Menu {
                     System.out.println("Not a valid attribute, try again");
             }
         }
-        return scanner.nextLine();
+        return sc.nextLine();
     }
+
+    // Process Methods
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static ArrayList<String> readFile(String filepath) {
+        ArrayList<String> data = new ArrayList<String>();
+        try {
+            Scanner scanner = new Scanner(new File(filepath));
+
+            while (scanner.hasNextLine()) {
+                data.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static Character parseAttributesFromString(String line, CharacterList list) {
+        String[] attributes = line.split(", ");
+        int id = 0;
+        String name = "";
+        String classification = "";
+        String race = "";
+        int str = 0;
+        int dex = 0;
+        int con = 0;
+
+        switch (attributes.length) {
+            case 7:
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(attributes[2]);
+                race = getValidRace(attributes[3]);
+                str = Integer.parseInt(attributes[4]);
+                dex = Integer.parseInt(attributes[5]);
+                con = Integer.parseInt(attributes[6]);
+                break;
+            case 6:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(attributes[2]);
+                race = getValidRace(attributes[3]);
+                str = Integer.parseInt(attributes[4]);
+                dex = Integer.parseInt(attributes[5]);
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+            case 5:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(attributes[2]);
+                race = getValidRace(attributes[3]);
+                str = Integer.parseInt(attributes[4]);
+                dex = getValidAbilityScore("dex", inputAttribute("dex"));
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+            case 4:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(attributes[2]);
+                race = getValidRace(attributes[3]);
+                str = getValidAbilityScore("str", inputAttribute("str"));
+                dex = getValidAbilityScore("dex", inputAttribute("dex"));
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+            case 3:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(attributes[2]);
+                race = getValidRace(inputAttribute("race"));
+                str = getValidAbilityScore("str", inputAttribute("str"));
+                dex = getValidAbilityScore("dex", inputAttribute("dex"));
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+            case 2:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(attributes[1], list);
+                classification = getValidClassification(inputAttribute("classification"));
+                race = getValidRace(inputAttribute("race"));
+                str = getValidAbilityScore("str", inputAttribute("str"));
+                dex = getValidAbilityScore("dex", inputAttribute("dex"));
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+            case 1:
+                System.out.println("> Not all attributes were complete, please re-enter the following.");
+                id = getValidId(attributes[0], list);
+                name = getValidName(inputAttribute("name"), list);
+                classification = getValidClassification(inputAttribute("classification"));
+                race = getValidRace(inputAttribute("race"));
+                str = getValidAbilityScore("str", inputAttribute("str"));
+                dex = getValidAbilityScore("dex", inputAttribute("dex"));
+                con = getValidAbilityScore("con", inputAttribute("con"));
+                break;
+        }
+
+        return new Character(id, name, classification, race, str, dex, con);
+    }
+
     // Validation Methods
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static boolean validateImportedString(String line) {
+        String[] data = line.split(", ");
+        if (data.length > 7) {
+            System.out.println("> There are too many comma delimiters, this line will be skipped");
+            return false;
+        } else if (data.length < 1) {
+            System.out.println("> No attributes detected, this line will be skipped");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
     public static boolean validateId(String id) {
         // Check that it's integer
         try {
@@ -97,92 +248,140 @@ public class Menu {
         }
     }
 
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static boolean validateName(String name) {
+        if (name.length() < 2) {
+            System.out.println("> Please enter a name with 2 characters or more");
+            return false;
+        } else if (name.length() > 50) {
+            System.out.println("> Please enter a name with 50 characters or less");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static boolean validateAbilityScore(String score) {
+        try {
+            int intScore = Integer.parseInt(score);
+            if (intScore < 3) {
+                System.out.println("> Please enter a score of 3 or more");
+                return false;
+            } else if (intScore > 20) {
+                System.out.println("> Please enter a score of 20 or less");
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("> Please enter an integer");
+            return false;
+        }
+    }
+
+    // Check for Duplication Methods
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
     public static boolean checkDuplicateIds(String id, CharacterList list) {
         int intId = Integer.parseInt(id);
         int listId = 0;
         int listSize = list.getListSize();
-        System.out.println("> List Size: " + listSize);
         if (listSize > 0) {
-            for (int i = 0; i < listSize + 1; i++) {
+            for (int i = 0; i < listSize; i++) {
                 listId = list.getCharacter(i).getId();
-                System.out.println("> Checking new ID: " + intId + " with " + listId);
                 if (listId == intId) {
                     System.out.println("> This ID already exists, please enter a new one");
                     return false;
-                } else {
-                    return true;
                 }
             }
         }
         return true;
     }
 
-    public static int getValidId(String id, CharacterList list, Scanner scanner) {
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static boolean checkDuplicateNames(String name, CharacterList list) {
+        String listName = "";
+        int listSize = list.getListSize();
+        if (listSize > 0) {
+            for (int i = 0; i < listSize; i++) {
+                listName = list.getCharacter(i).getName();
+                if (listName.equals(name)) {
+                    System.out.println("> This name already exists, please enter a new one");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Custom Get Methods
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+
+
+    /**
+     * --method:
+     * --parameters:
+     * --return:
+     * --purpose:
+     */
+    public static int getValidId(String id, CharacterList list) {
         while (true) {
             if (validateId(id)) {
                 if (checkDuplicateIds(id, list)) {
                     break;
                 } else {
-                    id = inputAttribute("id", scanner);
+                    id = inputAttribute("id");
                 }
             } else {
-                id = inputAttribute("id", scanner);
+                id = inputAttribute("id");
             }
         }
         return Integer.parseInt(id);
     }
 
-
     /**
      * --method:
      * --parameters:
      * --return:
      * --purpose:
      */
-    public static int getValidId(Scanner scanner) {
-        int id;
-        System.out.print("ID: ");
+    public static String getValidName(String name, CharacterList list) {
         while (true) {
-            // Validate input is an integer
-            if (scanner.hasNextInt()) {
-                id = scanner.nextInt();
-
-                // Validate input meets id constraints
-                if (id >= 1 && id <= 10) { // --Change to check for duplicates
-                    scanner.nextLine();
+            if (validateName(name)) {
+                if (checkDuplicateNames(name, list)) {
                     break;
                 } else {
-                    System.out.print("Please enter an integer between 1 and 10: ");
+                    name = inputAttribute("name");
                 }
             } else {
-                System.out.print("Please enter an integer: ");
-                scanner.next();
+                name = inputAttribute("name");
             }
         }
-        return id;
-    }
 
-    /**
-     * --method:
-     * --parameters:
-     * --return:
-     * --purpose:
-     */
-    public static String getValidName(Scanner scanner) {
-        String name;
-        System.out.print("Name: ");
-        while (true) { // --add checking for duplicates
-            // Validate input is a string
-            name = scanner.nextLine();
-            // Validate input meets name constraints
-            if (name.length() < 2) {
-                System.out.print("Enter a name with 2 characters or more: ");
-            } else if (name.length() > 50) {
-                System.out.print("Enter a name with 50 characters or less: ");
-            } else {
-                break;
-            }
-        }
         return name;
     }
 
@@ -192,25 +391,10 @@ public class Menu {
      * --return:
      * --purpose:
      */
-    public static String getValidClassification(Scanner scanner) {
-        String input;
-        String classification = "None selected";
-
-        System.out.println("Choose from one of the following classes:");
-        System.out.println("1. Barbarian");
-        System.out.println("2. Fighter");
-        System.out.println("3. Ranger");
-        System.out.println("4. Rogue");
-        System.out.println("5. Sorcerer");
-        System.out.println("6. Warlock");
-        System.out.println("7. Wizard");
-        System.out.println();
-        System.out.print("Enter class: ");
-
+    public static String getValidClassification(String classification) {
         boolean loop = true;
         while (loop) {
-            input = scanner.nextLine();
-            switch (input) {
+            switch (classification) {
                 case "1", "Barbarian", "barbarian":
                     classification = "Barbarian";
                     loop = false;
@@ -240,7 +424,8 @@ public class Menu {
                     loop = false;
                     break;
                 default:
-                    System.out.print("Enter a valid classification: ");
+                    System.out.println("> Invalid");
+                    classification = inputAttribute("classification");
             }
         }
         return classification;
@@ -252,24 +437,10 @@ public class Menu {
      * --return:
      * --purpose:
      */
-    public static String getValidRace(Scanner scanner) {
-        String input;
-        String race = "None selected";
-
-        System.out.println("Choose from one of the following races:");
-        System.out.println("1. Dwarf");
-        System.out.println("2. Dragonborn");
-        System.out.println("3. Elf");
-        System.out.println("4. Gnome");
-        System.out.println("5. Halfling");
-        System.out.println("6. Human");
-        System.out.println();
-        System.out.print("Enter class: ");
-
+    public static String getValidRace(String race) {
         boolean loop = true;
         while (loop) {
-            input = scanner.nextLine();
-            switch (input) {
+            switch (race) {
                 case "1", "Dwarf", "dwarf":
                     race = "Dwarf";
                     loop = false;
@@ -295,7 +466,8 @@ public class Menu {
                     loop = false;
                     break;
                 default:
-                    System.out.print("Enter a valid race: ");
+                    System.out.println("> Invalid");
+                    race = inputAttribute("race");
             }
         }
         return race;
@@ -307,34 +479,14 @@ public class Menu {
      * --return:
      * --purpose:
      */
-    public static int getValidAbilityScore(String ability, Scanner scanner) {
-        int score;
-        if (ability.equals("str")) {
-            System.out.print("Strength: ");
-        } else if (ability.equals("dex")) {
-            System.out.print("Dexterity: ");
-        } else if (ability.equals("con")) {
-            System.out.print("Constitution: ");
-        } else {
-            System.out.println("Not a valid ability");
-        }
+    public static int getValidAbilityScore(String ability, String score) {
         while (true) {
-            // Validate input is an integer
-            if (scanner.hasNextInt()) { // --need to validate for user entering ctrl+d/ctrl+z, it means end the input, but shouldn't quit the program
-                score = scanner.nextInt();
-
-                // Validate input meets id constraints
-                if (score >= 3 && score <= 20) { // --Change to check for duplicates
-                    scanner.nextLine();
-                    break;
-                } else {
-                    System.out.print("Please enter an integer from 3 to 20: ");
-                }
+            if (validateAbilityScore(score)) {
+                break;
             } else {
-                System.out.print("Please enter an integer: ");
-                scanner.next();
+                score = inputAttribute(ability);
             }
         }
-        return score;
+        return Integer.parseInt(score);
     }
 }
